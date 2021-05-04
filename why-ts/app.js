8592e76b1c7e3a89eb1de67cdc6308dba99477bc
swanious <ohsuwan1@gmail.com>
2021-05-05 01:29:42 +0900

    1. fetch users from api

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,5 +1,5 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = 'https://jsonplaceholder.typicode.com/users';
 
 // dom
 var username = document.querySelector('#username');
@@ -13,9 +13,16 @@ function startApp() {
   axios
     .get(url)
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+
+      // 만약 address를 adres처럼 잘못기입한다면 undefined를 반환함
+      // javascript는 브라우저에 띄우기 이전에는 이런 오류를 제대로 알기 힘듬
+      address.innerText = user[0].address;
     })
     .catch(function (error) {
       console.log(error);
