3ba59041217d1d1b424ebd861a2cb4012d1d6456
swanious <ohsuwan1@gmail.com>
2021-05-05 16:54:52 +0900

    know how to use @ts-check

---
+++ why-ts/sample.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ */
+
+// ts-check 를 통해서 javascript에서도 ts와 같은 문법검사를 할 수 있다.
+// @ts-check
+
+
+function sum(a, b) {
+  return a + b
+}
+
+sum(10, '20'); // 문법 오류
---
+++ why-ts/sample.ts
@@ -0,0 +1,5 @@
+function summ(a:number, b:number) {
+  return a + b;
+}
+
+const result = sum(10, 20)
