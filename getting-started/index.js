4e97df0ceb103e7eb3b80598323312938ae79520
swanious <ohsuwan1@gmail.com>
2021-05-05 17:02:37 +0900

    install typescript - npm i typescript -g - convert .ts into .js using tsc command

---
+++ getting-started/index.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(10, 30);
+console.log(result);
---
+++ getting-started/index.ts
@@ -0,0 +1,6 @@
+function sum(a: number, b: number):number {
+  return a + b;
+}
+
+const result = sum(10, 30);
+console.log(result);
